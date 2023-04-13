f07b635e98ae22dc30688439c23f204b644fba32
Benjamin Breton <benjamin.breton@ooshot.com>
2023-04-13 09:25:18 +0200

    went back one version of sanic

--- test.py
+++ test.py
@@ -3,8 +3,8 @@
 
 import requests
 
-model_inputs = {'prompt': 'Hello I am a [MASK] model.'}
+model_inputs = {"prompt": "Hello I am a [MASK] model."}
 
-res = requests.post('http://localhost:8000/', json = model_inputs)
+res = requests.post("http://localhost:8000/", json=model_inputs)
 
 print(res.json())
