02e9edd8e2fa8444c2bbd9055004d0d7df5d7ea1
Benjamin Breton <benjamin.breton@ooshot.com>
2023-04-13 19:17:01 +0200

    added dolly8B

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from instruct_pipeline import InstructionTextGenerationPipeline
 
 import torch
+import time
 
 app = Potassium("my_app")
 
@@ -13,17 +14,15 @@ app = Potassium("my_app")
 def init():
     device = 0 if torch.cuda.is_available() else -1
     MODEL_NAME = "databricks/dolly-v2-2-8b"
-    
+
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, padding_side="left")
     model = AutoModelForCausalLM.from_pretrained(
         MODEL_NAME,
         torch_dtype=torch.bfloat16,
         device_map="auto",
     )
-    
-    pipeline = InstructionTextGenerationPipeline(model=model, tokenizer=tokenizer)
 
-    context = {"pipeline": pipeline}
+    context = {"model": model, "tokenizer": tokenizer}
 
     return context
 
@@ -31,13 +30,49 @@ def init():
 # @app.handler is an http post handler running for every call
 @app.handler()
 def handler(context: dict, request: Request) -> Response:
+    model = context.get("model")
+    tokenizer = context.get("tokenizer")
+
+    # Start timer
+    t_1 = time.time()
+
     prompt = request.json.get("prompt")
-    pipeline = context.get("pipeline")
+
+    do_sample = request.json.get("do_sample", True)
+    max_new_tokens = request.json.get("max_new_tokens", 256)
+    top_p = request.json.get("top_p", 0.92)
+    top_k = request.json.get("top_k", 0)
+
+    # Create pipeline with the following parameters
+    pipeline = InstructionTextGenerationPipeline(
+        model=model,
+        tokenizer=tokenizer,
+        do_sample=do_sample,
+        max_new_tokens=int(max_new_tokens),
+        top_p=float(top_p),
+        top_k=int(top_k),
+    )
+    t_2 = time.time()
 
     # Run the model
     result = pipeline(prompt)
 
-    return Response(json={"outputs": result}, status=200)
+    t_3 = time.time()
+
+    return Response(
+        json={
+            "output": result,
+            "prompt": prompt,
+            "do_sample": do_sample,
+            "max_new_tokens": max_new_tokens,
+            "top_p": top_p,
+            "top_k": top_k,
+            "load_time": t_2 - t_1,
+            "generation_time": t_3 - t_2,
+            "inference_time": t_3 - t_1,
+        },
+        status=200,
+    )
 
 
 if __name__ == "__main__":
